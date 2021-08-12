e0f0181e22cca94b6459b1299c5965d4fab50de1
Claudio Torcato <claudiotorcato@gmail.com>
2021-08-12 08:46:52 -0300

    Escudos da Premier League e acréscimo nos comandos personalizados

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # palpites
+
+Após você baixar o código via git clone, será necessário realizar algumas ações:
+
+1) Criar um arquivo .env na pasta raiz para adicionar as seguintes variáveis
+
+FLASK_APP=palpites
+FLASK_ENV=development
+SECRET_KEY=<coloque aqui uma senha complexa>
+SQLALCHEMY_DATABASE_URI=sqlite:///banco.db
+
+2) Criar um ambiente virtual com o seguinte comando na pasta raiz
+
+$ python -m venv env
+$ source env/bin/activate
+
+3) Com o ambiente ativado com o comando anterior, crie as tabelas do banco de dados:
+
+$ flask init-database
+
+4) Cadastre os jogadores
+
+$ flask new-player "Nome do Jogador"
+
+5) Carregue os times pela liga
+
+$ flask load-teams serie_a_2021
--- palpites/ext/cli.py
+++ palpites/ext/cli.py
@@ -2,7 +2,7 @@ import click
 
 from palpites.ext.database import db, Time, Jogador
 
-eventos = {
+EVENT0S = {
     'serie_a_2021': [
         ('América-MG','america'), ('Athletico-PR','athletico'), ('Atlético-GO','cag'),
         ('Atlético-MG','atletico_mg'), ('Bahia', 'bahia'), ('Bragantino', 'bragantino'),
@@ -11,8 +11,25 @@ eventos = {
         ('Fortaleza', 'fortaleza'), ('Grêmio', 'gremio'), ('Internacional', 'inter'),
         ('Juventude', 'juventude'), ('Palmeiras', 'palmeiras'), ('Santos', 'santos'),
         ('São Paulo', 'spfc'), ('Sport Recife', 'sport')
+    ],
+    'premier2021': [
+        ('Arsenal', 'arsenal'), ('Aston Villa', 'aston_villa'),
+        ('Brenford', 'brentford'), ('Brighton', 'brighton'),
+        ('Burnley', 'burnley'), ('Chelsea', 'chelsea'),
+        ('Crystal Palace', 'crystal_palace'), ('Everton', 'everton'),
+        ('Leeds United', 'leeds'), ('Leicester City', 'leicester'),
+        ('Liverpool', 'liverpool'), ('Manchester City', 'man_city'),
+        ('Manchester United', 'man_united'), ('Newcastle', 'newcastle'),
+        ('Norwich', 'norwich'), ('Southampton', 'southampton'),
+        ('Tottenham', 'tottenham'), ('Watford', 'watford'),
+        ('West Ham', 'west_ham'), ('Wolves', 'wolves')
     ]
 }
+
+DESCRICOES = [
+    ('serie_a_2021', 'Clubes da Série A do Campeonato Brasileiro 2021'),
+    ('premier2021', 'Clubes da Premier League 2021-2022')
+]
 
 
 def init_app(app):
@@ -24,12 +41,14 @@ def init_app(app):
     @app.cli.command('load-teams')
     @click.argument('event')
     def carregar_times(event):
-        times = eventos.get(event)
+        times = EVENT0S.get(event)
         if times:
             for time in times:
                 db.session.add(Time(nome=time[0], sigla=time[1]))
             db.session.commit()
         else:
+            for descricao in DESCRICOES:
+                print('{} - {}'.format(descricao[0], descricao[1]))
             print('Evento não encontrado.')
 
     @app.cli.command('new-player')
@@ -38,3 +57,8 @@ def init_app(app):
         db.session.add(Jogador(nome=name))
         db.session.commit()
         print(f'Jogador {name} criado.')
+
+    @app.cli.command('events')
+    def listar_eventos():
+        for descricao in DESCRICOES:
+            print('{} - {}'.format(descricao[0], descricao[1]))
